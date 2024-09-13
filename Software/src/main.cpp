ce93195cec7bb0161f056b73c5fbff14f3d851f0
Joseph Vitale <Joseph.vitale@outlook.com>
2024-09-13 10:09:18 -0400

    Adding headers to main.cpp

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
+#include <Button.h>
+#include <Buzzer.h>
+#include <Scale.h>
 
 // put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
