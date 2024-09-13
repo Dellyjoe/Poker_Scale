33239021b27c4a5e1af150f34b9b892e9609958a
Joseph Vitale <Joseph.vitale@outlook.com>
2024-09-13 10:33:09 -0400

    Teensy 3.6 hello world Changed board type to tennsy 3.6 Added LED Blink for 1sec

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -8,10 +8,18 @@
 
 void setup() {
   // put your setup code here, to run once:
-  
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
-
+  // Turn LED on
+  digitalWrite(13, HIGH);
+  // Wait for 1 second
+  delay(1000);
+  // Turn LED off
+  digitalWrite(13, LOW);
+  // Wait for 1 second
+  delay(1000);
 }
